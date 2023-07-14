8db4db5899c0a39998fb14c795cf94787635f6c5
Nour1311 <souici.mnh.991@gmail.com>
2023-07-14 16:44:45 -0700

    My c programme  commit

---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo 'this is a readme file of the folder of the project hello world'
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("ce fichier contient le code de mon premier programme c.\n ");
+    return (0);
+}
