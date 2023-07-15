6e89b7ecd805607d1142f18406427e0b9c85a289
Nour1311 <souici.mnh.991@example.com>
2023-07-15 03:57:08 -0700

    c programmin  commit

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    printf("ce fichier contient le code de mon premier programme c.\n ");
-    return (0);
+return (0);
 }
