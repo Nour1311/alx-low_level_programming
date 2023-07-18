1a66d204bd68fe8325e5618d70094d524e6bff6d
Nour1311 <souici.mnh.991@gamil.com>
2023-07-18 15:59:09 -0700

    alphabet x10 commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	int j;
 
 	for (i = 0; i <= 9; i++)
 	{
-		print_alphabet();
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
