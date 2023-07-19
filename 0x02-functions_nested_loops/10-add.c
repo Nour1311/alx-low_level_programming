0337ed38eb4030144010fb79bbfe49426773b9ca
Nour1311 <souici.mnh.991@gamil.com>
2023-07-19 16:38:50 -0700

    sum commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers.
+ * @a: an integer.
+ * @b: an integer.
+ *
+ * Return: the the sum of two integers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
