3bd1f759c2200aa1d5c51c71cfcf42206b39de6b
Nour1311 <souici.mnh.991@gamil.com>
2023-07-19 14:42:48 -0700

    last digit commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @n: an integer.
+ *
+ * Return: the last digit of n.
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	x =  n % 10;
+	_putchar(x);
+	return (x);
+}
