ae47249e5861213d1b73746969d411adfbebcf14
Nour1311 <souici.mnh.991@gamil.com>
2023-07-24 16:03:34 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,24 +9,18 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n <= 0)
+	if (n == 1)
 	{
-		printf(" ");
+		printf("%d", a[0]);
 	}
 	else
 	{
-		if (n == 1)
+		printf("%d", a[0]);
+		for (i = 1; i < n - 1; i++)
 		{
-			printf("%d", a[0]);
+			printf(", %d", a[i]);
 		}
-		else
-		{
-			printf("%d", a[0]);
-			for (i = 1; i < n - 1; i++)
-			{
-				printf(", %d", a[i]);
-			}
-		}
+	}
 		putchar('\n');
 	}
 }
