dda6ccb02ad09f8507ab045bbb56be54dd075684
Nour1311 <souici.mnh.991@gamil.com>
2023-07-31 15:01:41 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: a string
+ * @c: a string
+ * Return: a new string
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while ((s[i] != c) && (s[i] != '\0'))
+		i++;
+	if (s[i] == c)
+		return (i);
+	else
+		return (NULL);
+}
