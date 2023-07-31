fbb55f00a8307517d8373fc20b07ad25b477c435
Nour1311 <souici.mnh.991@gamil.com>
2023-07-31 15:15:32 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,8 +9,10 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while ((s[i] != c) || (s[i] != '\0'))
+	while ((s[i] != c) && (s[i] != '\0'))
+	{
 		i++;
+	}
 	if (s[i] == c)
 		return (s + i);
 	else
