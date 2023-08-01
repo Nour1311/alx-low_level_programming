9dee4b375096d0a9fd45354c59b2d57b157863ef
Nour1311 <souici.mnh.991@gamil.com>
2023-08-01 07:12:18 -0700

    task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,11 @@
+#include "main.h"
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positif",i);
+	else if (i < 0)
+		printf("%d is negatif",i);
+		else 
+			printf("%d is zero",i);
+
+}
