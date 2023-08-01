4c9da18ce235417e4a3e29003eaaa3664ffc598e
Nour1311 <souici.mnh.991@gamil.com>
2023-08-01 05:41:46 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,21 +8,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int bytes = 0;
-	int i;
+	unsigned int b = 0;
+	int i = 0;
 
 	while (*s)
 	{
-		for (i = 0; accept[i] != '\0'; i++)
+		for (; accept[i] ; i++)
 		{
 			if (accept[i] == *s)
 			{
-				bytes++;
+				b++;
 				break;
 			}
+			else if ((accept[i + 1]) == '\0')
+				return (b);
 		}
-		return (bytes);
 		s++;
 	}
-	return (bytes);
+	return (b);
 }
