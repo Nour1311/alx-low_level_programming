01ed74b37e7cd904f1c18dd5845ac9807b6dca90
Nour1311 <souici.mnh.991@gamil.com>
2023-08-03 14:08:23 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,7 +10,7 @@ int factorial(int n)
 {
 	if (n < 0)
 	{
-		return (-1);	
+		return (-1);
 	}
 	else
 	{
@@ -20,7 +20,7 @@ int factorial(int n)
 		}
 		else
 		{
-			return (factorial(n) * factorial(n - 1));
+			return (n * factorial(n - 1));
 		}
 	}
 }
