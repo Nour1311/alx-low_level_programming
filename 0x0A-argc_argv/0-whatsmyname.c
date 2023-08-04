5374c9593148b97fcff5565efcd2c71d028da788
Nour1311 <souici.mnh.991@gamil.com>
2023-08-04 15:07:14 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: an ineger the length of the array.
+ * @argv: an array contain the instruction of the program.
+ */
+void main(int argc, char argv[])
+{
+	printf("%d", argv[0]);
+}
