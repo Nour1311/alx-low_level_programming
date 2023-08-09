637a63183fd0afe6899ae446f64323e4f8fc0264
Nour1311 <souici.mnh.991@gamil.com>
2023-08-09 02:07:10 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,8 +3,11 @@
  * main - prints its name, followed by a new line.
  * @argc: an ineger the length of the array.
  * @argv: an array contain the instruction of the program.
+ * Return: always 0.
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("%s \n", argv[0]);
+	return (0);
 }
