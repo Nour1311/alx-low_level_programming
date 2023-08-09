fa7ade79a0156464452db39eeca4406acf8dbe54
Nour1311 <souici.mnh.991@gamil.com>
2023-08-09 03:39:23 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,10 @@
  * @argv: array of arguments
  * Return: Always 0 (Success)
  */
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
+
 	printf("%s \n", argv[0]);
 	return (0);
 }
