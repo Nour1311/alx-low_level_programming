bcfd5cbda350aa2a7cb7d1f025a69401f757cf55
Nour1311 <souici.mnh.991@gamil.com>
2023-08-09 02:20:14 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
  * main - prints its name, followed by a new line.
@@ -5,10 +6,8 @@
  * @argv: an array contain the instruction of the program.
  * Return: always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	(void)argc;
-
 	printf("%s \n", argv[0]);
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,1 +1,1 @@
-int main(int argc, char *argv[]);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
