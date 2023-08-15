31d12b5c147d87fee9eb30a2b65793496010975f
Nour1311 <souici.mnh.991@gamil.com>
2023-08-15 15:01:21 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees the dogs..
+ * @d:the struct.
+ * Return: Nothing.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+		free(d);
+	}
+}
