d9a9a3c2b81734c8c5aa0c8811b585b538bfeb72
Nour1311 <souici.mnh.991@gamil.com>
2023-08-15 04:35:39 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct dog - dog's description
+ * @name: dog's name
+ * @age: dog's age
+ * @owner:  the owener of the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
