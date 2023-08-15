cccdb3ba5cd1a636fcfc533db21d6db7233656d2
Nour1311 <souici.mnh.991@gamil.com>
2023-08-15 04:54:06 -0700

    task 0

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * init_dog: initialize a parametre of a struct.
+ * @d: structur's name.
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's owner.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	*d.name = name;
+	*d.age = age;
+	*d.owner = owner;
+}
