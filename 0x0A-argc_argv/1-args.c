6814dda5f9fc2d48532fb575f32ddbd27bfe816b
Nour1311 <souici.mnh.991@gamil.com>
2023-08-16 06:38:55 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main_args - prints number of arguments
+ * @argc: number of arguments.
+ * @argv: array of arguments
+ */
+void  main_args(int argc, char *argv[] __attribute__((__unused__)))
+{
+	printf("%d\n", argc);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 int main(int argc __attribute__((__unused__)), char *argv[]);
+void main_args(int argc, char *argv[] __attribute__((__unused__)));
 
 #endif
