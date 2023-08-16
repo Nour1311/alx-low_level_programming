d7d07c6846e829d9ae5e719a1193ca715a08a88c
Nour1311 <souici.mnh.991@gamil.com>
2023-08-16 06:43:59 -0700

    task 1

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,6 @@
 
 int _putchar(char c);
 int main(int argc __attribute__((__unused__)), char *argv[]);
-void main_args(int argc, char *argv[] __attribute__((__unused__)));
+int main_args(int argc, char *argv[] __attribute__((__unused__)));
 
 #endif
