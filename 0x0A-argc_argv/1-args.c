7cfce7f4fb512db7c1c38a337cc82168314b05f3
Nour1311 <souici.mnh.991@gamil.com>
2023-08-16 06:42:40 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,8 +4,10 @@
  * main_args - prints number of arguments
  * @argc: number of arguments.
  * @argv: array of arguments
+ * Return: always 0.
  */
-void  main_args(int argc, char *argv[] __attribute__((__unused__)))
+int main_args(int argc, char *argv[] __attribute__((__unused__)))
 {
 	printf("%d\n", argc - 1);
+	return (0);
 }
