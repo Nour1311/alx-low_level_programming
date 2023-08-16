698da5f41caf49c9a6bd20f47fc4a9a9cc8650f8
Nour1311 <souici.mnh.991@gamil.com>
2023-08-16 05:24:37 -0700

    task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,31 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * identique - compares two strings .
- * @x: a string.
- * @y: a string.
- * Return: returns 1 if the strings can be considered identical.
- * otherwise return 0.
- */
-int identique(char *x, char *y,int conteur)
-{
-	if (*x == '*')
-		x++;
-	else
-
-		if (*y == '*')
-			y++;
-	if (*x == *y)
-	{
-
-		identique(x++, y++);
-		return (1);
-	}
-	else 
-		return (0);
-}
-
-/**
  * wildcmp - compares two strings .
  * @s1: a string.
  * @s2: a string.
