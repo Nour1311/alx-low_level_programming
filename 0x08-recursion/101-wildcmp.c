c9a494c8e526a30973f03aa0143b0263ed48d0ad
Nour1311 <souici.mnh.991@gamil.com>
2023-08-16 05:38:22 -0700

    task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,12 +16,12 @@ int wildcmp(char *s1, char *s2)
 
 	else if (*s1 == *s2)
 	{
-		return (wildcmp(s1++, s2++));
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 
 	if (*s2 == '*')
 	{
-		if (wildcmp(s1, s2++))
+		if (wildcmp(s1, s2 + 1))
 		{
 			return (1);
 		}
@@ -31,7 +31,7 @@ int wildcmp(char *s1, char *s2)
 			return (0);
 		}
 
-		if (wildcmp(s1++, s2))
+		if (wildcmp(s1 + 1, s2))
 		{
 			return (1);
 		}
