4b04342d2df4adcfc644c3754e27abae9fda3b24
Nour1311 <souici.mnh.991@gamil.com>
2023-08-17 02:53:02 -0700

    task 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	va_start(args, int);
+	va_start(args, n);
 	for (i = 0; i < n - 1; i++)
 		printf("%d%s\n", va_arg(args, void)separator);
 	print("%d\n", va_arg(args, void));
