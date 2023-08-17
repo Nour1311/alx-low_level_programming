ec6f5f8e5b70b4959f53a4ffdf411f363e4729bc
Nour1311 <souici.mnh.991@gamil.com>
2023-08-17 02:22:46 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,11 +11,11 @@ int sum_them_all(const unsigned int n, ...)
 	int somme = 0;
 	va_list args;
 
+	if (n == 0)
+		return (0);
 	va_start(args, n);
 	for (i = 0; i < n; i++)
-	{
 		somme = somme + va_arg(args, int);
-	}
 	va_end(args);
 	return (somme);
 }
